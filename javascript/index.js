7e2540c438198d22ecb2f172433a186bbb52dfc1
ev1cka <142107311+eva-coding@users.noreply.github.com>
2024-04-07 13:08:09 +0100

    Added JS to show current date and time in 3 different locations

---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Prague
+  let pragueElement = document.querySelector("#prague");
+  let pragueDateElement = pragueElement.querySelector(".date");
+  let pragueTimeElement = pragueElement.querySelector(".time");
+  let pragueTime = moment().tz("Europe/Prague");
+
+  pragueDateElement.innerHTML = pragueTime.format("MMMM Do YYYY");
+  pragueTimeElement.innerHTML = pragueTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
